50164577fa5ef4e0039d01e440e2c2cbac086016
demonlibra <demonlibra@inbox.ru>
2020-02-16 23:18:35 +0300

    Some fixes

--- airnef_canon_g9x.sh
+++ airnef_canon_g9x.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-# Скрипт создает Wi-Fi точку доступа, к которой подключается фотоаппарат, и скачивает фото/видео программой Airnef
+# Сценарий создает Wi-Fi точку доступа, к которой подключается фотоаппарат, и скачивает фото/видео программой Airnef
 
 # !!! Важное примечание !!! Необходимо исключить строку 1137 в файле airnefcmd.py
 # http://testcams.com/blog/forums/topic/canon-eos-m5-connection-refused/
 
+#----------------------------------- Параметры -----------------------------------------------------------------------------------------------
+
 adapter="wlp2s0"											# Имя Wi-Fi адаптера (можно определить в терминале командой ifconfig)
-ssid="demonlibra-hotspot"									# Имя сети для создаваемой точки доступа
+ssid="demonlibra-hotspot"									# Имя сети для создаваемой точки доступа (задать на свое усмотрение)
 password="12345678"											# Пароль создаваемой точки доступа, который придется вводить в фотоаппарате
-canonmac="84:ba:3b:52:06:5a"								# MAC адрес фотоаппарата (можно найти в параметрах беспроводоной сети фотоаппарата)
-conname="hotspotcanon"										# Имя соединения в списке NetworkManager
-pathairnef="/home/demonlibra/App/airnef/airnefcmd.py"		# Путь к программе Airnef
-pathout="/mnt/data/Airnef"									# Путь для копирования файлов из фотоаппарата
-quality_jpg=85												# Процент сжатия фото
+canon_mac="84:ba:3b:52:06:5a"								# MAC адрес фотоаппарата (можно найти в параметрах беспроводоной сети фотоаппарата)
+hotspot_name="hotspotcanon"									# Имя соединения в списке NetworkManager (задать на свое усмотрение)
+timeout=60													# Время ожидания подключения
+
+pathairnef="$HOME/App/airnef/airnefcmd.py"					# Путь к файлу airnefcmd.py
+pathout="/mnt/data/Airnef"									# Путь сохранения фото/видео
+
+quality_jpg=85												# Процент сжатия фото после копирования
+
+power_from_battery="Adapter 0: on-line"						# Только для ноутбуков. Выполните в терминале "apci -a" при работе от батареи
+
+#---------------------------------------------------------------------------------------------------------------------------------------------
 
 # Включаем wifi, если выключен
 if [[ `nmcli radio wifi` = "disabled" ]]
@@ -25,44 +34,52 @@ if [[ `nmcli radio wifi` = "disabled" ]]
 if [[ `nmcli radio wifi` = "enabled" ]]
 	then
 		# Создаем точку доступа
-		nmcli device wifi hotspot ssid "$ssid" con-name "$conname" password "$password"
-		timeout=60
-		for ((i=$timeout; i>1; i--))
+		nmcli device wifi hotspot ssid "$ssid" con-name "$hotspot_name" password "$password"
+
+		for ((i=$timeout; i>1; i--))	# Цикл проверки подключения камеры к точке доступа
 			do
 				sleep 1
-				if [[ `iw dev $adapter station dump | grep $canonmac` ]]
+				if [[ `iw dev $adapter station dump | grep "$canon_mac"` ]]
 					then
-						check=`arp | grep $canonmac`
-						canonip=`echo ${check%%" "*}`
+						check=`arp | grep $canon_mac`
+						canon_ip=`echo ${check%%" "*}`
+						
+						if [[ "$canon_ip" ]]
+							then
+								echo; echo "IP адрес камеры: "$canon_ip
+								
+								# Запускаем airnef и копируем файлы из фотоаппарата в каталог pathout
+								python "$pathairnef" --ipaddress "$canon_ip" --outputdir "$pathout"
 
-						# Запускаем airnef и копируем файлы из фотоаппарата в каталог pathout
-						python $pathairnef --ipaddress $canonip --outputdir $pathout
-
-						# Сжатие новых фотографий
-						find "$pathout" -iname "*.JPG" | sort | while IFS= read -r file
-							do
-								if [[ `identify -format '%Q' "$file"` > 95 ]]
-									then 
-										# Сжимаем фото
-										echo; echo --------------------------------; echo
-										convert -quality "$quality_jpg" -verbose "$file" "$file"
-										echo; echo --------------------------------; echo;
-								fi
-							done
-							
-						i=0									# Обнуляем обратный отсчет подключения
-						nmcli connection down "$conname"	# Выключаем точку доступа
+								# Сжатие новых фотографий
+								find "$pathout" -iname "*.JPG" | sort | while IFS= read -r file
+									do
+										if [[ `identify -format '%Q' "$file"` -gt "$quality_jpg" ]] # Выполнять сжатие для файлов без сжатия
+											then 
+												# Сжимаем фото
+												echo; echo --------------------------------; echo
+												convert -quality "$quality_jpg" -verbose "$file" "$file"
+												echo; echo --------------------------------; echo;
+										fi
+									done
+												
+								i=0										# Обнуляем обратный отсчет подключения для завершения цикла
+								echo
+								nmcli connection down "$hotspot_name"	# Выключаем точку доступа
+						fi
 					else
+						clear
 						echo "# Включите фотоаппарат и выберите тип подключения Smartphone. Осталось секунд: $i"
 				fi
 			done
-			echo; echo --------------------------------; echo; echo "Скачивание и сжатие завершено"; echo; read -p "Нажмите ENTER чтобы закрыть окно"
-fi
+			
+			#Выключаем wifi, если питание от аккумулятора
+			if [[ `acpi -a` != "$power_from_battery" ]]
+				then
+					nmcli radio wifi off
+			fi
 
-#Выключаем wifi, если питание от аккумулятора
-if [[ `acpi -a` != "Adapter 0: on-line" ]]
-	then
-		nmcli radio wifi off
+			echo; echo --------------------------------; echo; echo "Скачивание и сжатие завершено"; echo; read -p "Нажмите ENTER чтобы закрыть окно"
 fi
 
 exit 0
